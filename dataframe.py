229d94dad017bd3015defb9a95a1e6a3a455ff7d
racchu123 <racchu123@gmail.com>
2023-09-01 12:46:08 +0530

    change6

---
+++ dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np 
+
+data=pd.DataFrame([[10,11,12,13],[10,11,12,13]],columns=["maths","seicence","kannada","english"])
+print(data)
+
+output
+
+maths  seicence  kannada  english
+0     10        11       12       13
+1     10        11       12       13
