94d1bf956b994d3677c2d96674e1e305035b03a6
racchu123 <racchu123@gmail.com>
2023-09-04 16:33:38 +0530

    change38

---
+++ uniriate.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as pl
+import seaborn as sns
+
+data=pd.read_csv("C:/Users/SPTINT-14/Desktop/dataset/titanic.csv")
+print(data)
+print(data.head(5))
+print(data.info())
+print(data.shape)
+print(data.describe())
+print(data['Age'].hist())
+sns.countplot(x='Pclass',data=data)
+print(data['Pclass'].value_counts())
+sns.countplot(x='Sex',data=data)
+print(data['Age'].value_counts().plot(kind='bar'))
+print(data['Age'].plot.density())
+print(sns.boxplot(x='Age',data=data))
+sns.countplot(x='Sex',data=data)
+
+output
+
+PassengerId  Survived  Pclass  ...     Fare Cabin  Embarked
+0              1         0       3  ...   7.2500   NaN         S
+1              2         1       1  ...  71.2833   C85         C
+2              3         1       3  ...   7.9250   NaN         S
+3              4         1       1  ...  53.1000  C123         S
+4              5         0       3  ...   8.0500   NaN         S
+..           ...       ...     ...  ...      ...   ...       ...
+886          887         0       2  ...  13.0000   NaN         S
+887          888         1       1  ...  30.0000   B42         S
+888          889         0       3  ...  23.4500   NaN         S
+889          890         1       1  ...  30.0000  C148         C
+890          891         0       3  ...   7.7500   NaN         Q
+
+[891 rows x 12 columns]
+   PassengerId  Survived  Pclass  ...     Fare Cabin  Embarked
+0            1         0       3  ...   7.2500   NaN         S
+1            2         1       1  ...  71.2833   C85         C
+2            3         1       3  ...   7.9250   NaN         S
+3            4         1       1  ...  53.1000  C123         S
+4            5         0       3  ...   8.0500   NaN         S
+
+[5 rows x 12 columns]
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 891 entries, 0 to 890
+Data columns (total 12 columns):
+ #   Column       Non-Null Count  Dtype
+---  ------       --------------  -----
+ 0   PassengerId  891 non-null    int64
+ 1   Survived     891 non-null    int64
+ 2   Pclass       891 non-null    int64
+ 3   Name         891 non-null    object
+ 4   Sex          891 non-null    object
+ 5   Age          714 non-null    float64
+ 6   SibSp        891 non-null    int64
+ 7   Parch        891 non-null    int64
+ 8   Ticket       891 non-null    object
+ 9   Fare         891 non-null    float64
+ 10  Cabin        204 non-null    object
+ 11  Embarked     889 non-null    object
+dtypes: float64(2), int64(5), object(5)
+memory usage: 83.7+ KB
+None
+(891, 12)
+       PassengerId    Survived      Pclass  ...       SibSp       Parch        Fare
+count   891.000000  891.000000  891.000000  ...  891.000000  891.000000  891.000000
+mean    446.000000    0.383838    2.308642  ...    0.523008    0.381594   32.204208
+std     257.353842    0.486592    0.836071  ...    1.102743    0.806057   49.693429
+min       1.000000    0.000000    1.000000  ...    0.000000    0.000000    0.000000
+25%     223.500000    0.000000    2.000000  ...    0.000000    0.000000    7.910400
+50%     446.000000    0.000000    3.000000  ...    0.000000    0.000000   14.454200
+75%     668.500000    1.000000    3.000000  ...    1.000000    0.000000   31.000000
+max     891.000000    1.000000    3.000000  ...    8.000000    6.000000  512.329200
+
+[8 rows x 7 columns]
+AxesSubplot(0.125,0.125;0.775x0.755)
+3    491
+1    216
+2    184
+
